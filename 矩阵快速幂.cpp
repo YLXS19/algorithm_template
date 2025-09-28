fb85b03cd81acf7f607d7ccb3acb9946fc8d05a7
YLXS19 <1355815270@qq.com>
2025-09-28 16:08:56 +0800

    Update 矩阵快速幂.cpp

--- 矩阵快速幂.cpp
+++ 矩阵快速幂.cpp
@@ -1,58 +1,46 @@
-//矩阵快速幂
-#include<bits/stdc++.h>
-using namespace std;
-int n,m,a[100][100],y[100][100],ans[100][100],an[100][100],f[100];
-string s;
-void multiply(){
-	int i,j,k;
-	for(i=0;i<n;i++){
-		for(j=0;j<n;j++){
-			int aws=0;
-			for(k=0;k<n;k++){
-				aws+=a[i][k]*y[k][j];
-				aws%=2;
-			}
-			an[i][j]=aws;
-		}
-	}
-}//矩阵乘法
-int main(){
-	while(scanf("%d",&m)==1){
-		cin>>s;
-		int i,j;
-		n=s.size();
-		memset(a,0,sizeof(a));
-		memset(ans,0,sizeof(ans));
-		ans[0][0]=a[0][0]=a[0][n-1]=1;
-		for(i=1;i<n;i++)ans[i][i]=a[i][i]=a[i][i-1]=1;
-		while(m){
-			if(m%2){
-				for(i=0;i<n;i++){
-					for(j=0;j<n;j++)y[i][j]=ans[i][j];
-				}
-				multiply();
-				for(i=0;i<n;i++){
-					for(j=0;j<n;j++)ans[i][j]=an[i][j];
-				}	
-			}
-			m/=2;
-			for(i=0;i<n;i++){
-				for(j=0;j<n;j++)y[i][j]=a[i][j];
-			}
-			multiply();
-			for(i=0;i<n;i++){
-				for(j=0;j<n;j++)a[i][j]=an[i][j];
-			}
-		}//快速幂
-		for(i=0;i<n;i++){
-			int aws=0;
-			for(j=0;j<n;j++){
-				aws+=ans[i][j]*(s[j]-'0');
-				aws%=2;
-			}
-			f[i]=aws;
-		}
-		for(i=0;i<n;i++)printf("%d",f[i]);
-		printf("\n");
-	}
+using matrix = vector<vector<long long>>;
+
+int MOD = 1e9 + 7; 
+
+matrix mul(matrix& a, matrix& b) {
+    int n = a.size(), m = b[0].size();
+    matrix c = matrix(n, vector<long long>(m));
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < a[i].size(); k++) {
+            if (a[i][k] == 0) {
+                continue;
+            }
+            for (int j = 0; j < m; j++) {
+                c[i][j] = (c[i][j] + a[i][k] * b[k][j]) % MOD;
+            }
+        }
+    }
+    return c;
 }
+
+matrix pow_mul(matrix a, int n, matrix& f1) {
+    matrix res = f1;
+    for(auto v : res) cout << v[0] << " ";
+    cout << endl;
+    while (n) {
+        if (n & 1) {
+            res = mul(a, res);
+            for(auto v : res) cout << v[0] << " ";
+            cout << endl;
+        }
+        a = mul(a, a);
+        n >>= 1;
+    }
+    return res;
+}
+
+class Solution {
+public:
+    int zigZagArrays(int n, int l, int r) {
+        
+	    matrix fn = pow_mul(mtr, n - 2, f1);
+
+        return fn[0][0] << 1;
+        
+    }
+};
